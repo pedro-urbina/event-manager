7df8e64d3d9178b185c96b2c34239669c055dc27
Pedro Urbina <purbina14@gmail.com>
2020-04-27 17:52:42 -0400

    Added CSV parser

--- src/io/github/pedro_urbina/eventmanager/EventManager.java
+++ src/io/github/pedro_urbina/eventmanager/EventManager.java
@@ -1,7 +1,28 @@
 package io.github.pedro_urbina.eventmanager;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
+import java.util.List;
+
 public class EventManager {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {
+        CSVUtils csvReader = new CSVUtils();
+
+        String csvFile = "event_attendees.csv";
+        int rowIndex = 0;
+
+        Scanner scanner = new Scanner(new File(csvFile));
+
+        while (scanner.hasNext()) {
+            List<String> line = csvReader.parseLine(scanner.nextLine());
+            if (rowIndex == 0) {
+                rowIndex++;
+                continue;
+            }
+            System.out.println("Attendee [name=" + line.get(2) + "]");
+        }
+        scanner.close();
     }
 }
